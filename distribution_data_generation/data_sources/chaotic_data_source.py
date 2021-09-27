3cb861811a484373be6da3be989d5f3f30abef16
Matthew Akram <mazfh85246@gmail.com>
2021-09-27 21:05:21 +0200

    Implemented chaotic data source

--- distribution_data_generation/data_sources/chaotic_data_source.py
+++ distribution_data_generation/data_sources/chaotic_data_source.py
@@ -7,5 +7,19 @@ class ChaoticDataSource(DataSource):
         super().__init__()
 
     @tf.function
-    def query(self, x: tf.Tensor):
-        """henon function. What?"""
+    def query(self, actual_query: tf.Tensor):
+
+        a = 1.3
+        one = 1
+        b = 0.3
+
+        out_x = [0.0, 0.0]
+
+        x_ustacked = tf.unstack(actual_query)
+        last_x = x_ustacked[1]
+        last_last_x = x_ustacked[0]
+        for i in x_ustacked[2:]:
+            out_x.append(one - a * last_x + b * last_last_x)
+            last_last_x = last_x
+            last_x = i
+        return tf.stack(out_x)
---
+++ tests/data_source/test_chaotic_data_source.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from distribution_data_generation.data_sources.chaotic_data_source import ChaoticDataSource
+import tensorflow as tf
+
+
+class TestChaoticDataSource(TestCase):
+    def test_query(self):
+        s = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0])
+        source = ChaoticDataSource()
+        actual = source.query(s)
+
+        print(actual)
