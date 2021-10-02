0e43a6e76374bc60d8e92b574d4c2a7c84824701
Matthew Akram <mazfh85246@gmail.com>
2021-10-02 18:29:59 +0200

    Added data_set_data_source.py

---
+++ distribution_data_generation/data_sources/data_set_data_source.py
@@ -0,0 +1,25 @@
+
+from distribution_data_generation.data_source import DataSource
+import tensorflow as tf
+
+
+class DataSetDataSource(DataSource):
+    """
+    This solution sucks. tf.dataset isn't meant for this, neither is tf..hashtable. This can be changed in the future,
+    i think there are tf based k-d-tree implementations one could use here
+    """
+    data_points = None
+    data_values = None
+
+    def __init__(self, data_points, data_values):
+        super().__init__()
+        self.data_points = data_points
+        self.data_values = data_values
+
+    def _query(self, actual_queries: tf.Tensor):
+        place = 0
+        for t in self.data_points:
+            if tf.reduce_all(tf.math.equal(t, actual_queries)):
+                break
+            place += 1
+        return actual_queries, self.data_values[place]
---
+++ tests/data_source/test_data_set_data_source.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+import tensorflow as tf
+from distribution_data_generation.data_sources.data_set_data_source import DataSetDataSource
+
+
+class TestDataSetDataSource(TestCase):
+
+    def test_query(self):
+        a = tf.constant([4.0, 4.0, 2.0, 2.0, 443.0, 3.0, 4.0, 344.0, 4.0])
+        value = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0])
+
+        b = tf.constant([4.0, 4.0, 2.0, 2.0, 443.0, 3.0, 4.0, 344.0, 4.0])  # b is but a different object
+
+        keys = [a, tf.random.uniform(a.shape), tf.random.uniform(a.shape), tf.random.uniform(a.shape)]
+        values = [value, tf.random.uniform(a.shape), tf.random.uniform(a.shape), tf.random.uniform(a.shape)]
+
+        source = DataSetDataSource(keys, values)
+
+        expected = value
+        x, actual = source.query([b])[0]
+
+        tf.assert_equal(expected, actual)
