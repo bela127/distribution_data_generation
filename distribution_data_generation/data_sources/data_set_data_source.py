51cdd522954764ec817aed0ad08bc48a463620b4
Matthew Akram <mazfh85246@gmail.com>
2022-01-16 14:43:53 +0100

    begun work on shape checking. Breaking changes

--- distribution_data_generation/data_sources/data_set_data_source.py
+++ distribution_data_generation/data_sources/data_set_data_source.py
@@ -11,25 +11,28 @@ class DataSetDataSource(DataSource):
     data_points = None
     data_values = None
 
-    def __init__(self, in_dim: int, data_points: tf.Tensor, data_values: tf.Tensor):
+    def __init__(self, data_points: tf.Tensor, data_values: tf.Tensor):
         """
         Creates basically a tensor dictionary.
         This version works with any dimension, but only the storing of 1-D tensors is allowed
 
-        TODO: remove in_dim, not necessary
-        :param in_dim: the dimension of the vectors that are to be stored
         :param data_points: A 2-D Tensor. Acts as a list of vectors
         :param data_values: A 2-D Tensor. Acts as a list of vectors. The first dimension of data_values must be the
                 same as that of data_points
         """
         self.data_points = data_points
         self.data_values = data_values
-        self.in_dim = in_dim
+
+        if not len(data_points.shape) == 2:
+            raise ValueError("Requires vector input")
+
+        self.point_shape = data_points[0].shape
+        self.value_shape = data_values[0].shape
         self.pool = None
 
     def post_init(self, retrievement_strategy: RetrievementStrategy):
         super().post_init(retrievement_strategy)
-        self.pool = DiscreteVectorPool(in_dim=self.in_dim, queries=self.data_points,
+        self.pool = DiscreteVectorPool(in_dim=self.point_shape[0], queries=self.data_points,
                                        find_streategy=self.retrievementStrategy)
 
     def query(self, actual_queries: tf.Tensor):
