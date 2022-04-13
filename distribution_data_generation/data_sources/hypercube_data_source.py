dfddfeb758a37b6a9f86f88815823cd27c35855f
Matthew Akram <mazfh85246@gmail.com>
2022-04-13 19:58:32 +0200

    fixed hypercube_data_source.py. All that is left is some documentation and a version bump

--- distribution_data_generation/data_sources/hypercube_data_source.py
+++ distribution_data_generation/data_sources/hypercube_data_source.py
@@ -8,29 +8,28 @@ from distribution_data_generation.data_source import DataSource
 
 
 class HypercubeDataSource(DataSource):
-    def __init__(self, dim: int):
-        self.pool = ContinuousVectorPool(dim=dim, ranges=[[(0, 1)]] * dim)
-
+    def __init__(self, dim: int, dependency_dim: int):
+        self.pool = ContinuousVectorPool(dim=dim, ranges=[[(0, 1)]] * (dim * dependency_dim))
+        self.dependency_dim = dependency_dim
         self.point_shape = (dim,)
         self.value_shape = (dim,)
 
     @tf.function
     def _query(self, actual_queries: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
-
         entries = tf.unstack(actual_queries)
         out = []
 
         for entry in entries:
             next_entry = None
 
-            rand = tf.random.uniform([])
-            f1 = lambda : rand
-            f2 = lambda : .0
-            f3 = lambda : 1.
+            special_dim = tf.random.uniform(shape=(), minval=0, maxval=self.dependency_dim, dtype=tf.int32)
 
-            next_entry = tf.case([(entry == 1., f1), (entry == .0, f1), (rand < .5, f2)], f3)
-
-            out.append(next_entry)
+            for i in range(0, self.dependency_dim):
+                if (entry == .0 or entry == 1.0) and i == special_dim:
+                    next_entry = tf.random.uniform([])
+                else:
+                    next_entry = tf.case([(tf.random.uniform([]) < 0.5, lambda: 1.0)], lambda: .0)
+                out.append(next_entry)
 
         return actual_queries, tf.stack(out)
 
